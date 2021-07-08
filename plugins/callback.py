6808b79c599f2561229eb2e0d2be723874f39a1b
oktest145 <83488344+oktest145@users.noreply.github.com>
2021-07-08 16:15:29 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -171,12 +171,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/Ironman_Cloud'),
+                InlineKeyboardButton('🤖 Other Bots', url='https://t.me/Ironman_Cloud'),
             ],
             [
-                InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+                InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/ironman_cloud_support'),
+                InlineKeyboardButton('🧩 Source', url='https://github.com/oktest145/MusicPlayer'),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
